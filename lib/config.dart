c11265553f039372aec086d2a56b8aecd32b9356
Garmode3073 <gauravgarmode16@gmail.com>
2021-09-03 15:36:55 +0530

    configuration file

---
+++ lib/config.dart
@@ -0,0 +1,8 @@
+class Config {
+  static String key = ""; //woocommerce api key
+  static String secret = ""; //woocommerce api secret
+  static String url =
+      ""; //wordpress site url in the format "https://someapp.domain.com/wp-json/wc/v3/"
+  static String customer =
+      ""; //this field will contain the simple string "customers"
+}
